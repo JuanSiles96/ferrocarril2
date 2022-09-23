cd3528b7a1f019c0796b58829b2e22a04f0eb878
JuanSiles96 <juanjosesiles3@gmail.com>
2022-09-23 11:24:27 -0400

    tren completo

---
+++ src/Locomotor.java
@@ -0,0 +1,14 @@
+public class Locomotor {
+    public Tren tren;
+    Integer codigo;
+    Tren tipo;
+
+    public Locomotor(Integer codigo, Tren tipo) {
+        this.codigo = codigo;
+        this.tipo = tipo;
+    }
+
+    public Locomotor(int codigo) {
+
+    }
+}
---
+++ src/Vagon.java
@@ -0,0 +1,15 @@
+public class Vagon {
+    Tren tren;
+    String tipo;
+    Integer capacidad;
+
+    public Vagon(Tren tren, String tipo, Integer capacidad) {
+        this.tren = tren;
+        this.tipo = tipo;
+        this.capacidad = capacidad;
+    }
+
+    public Vagon(String s, int i) {
+
+    }
+}
